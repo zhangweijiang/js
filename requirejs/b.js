d5044aedc646897b1d347e4506188e04358b8b39
张伟江 <982215226@qq.com>
2019-04-08 17:24:25 +0800

    测试

--- requirejs/b.js
+++ requirejs/b.js
@@ -2,3 +2,9 @@ var b = {};
 b.test = function(input){  
     alert("b.js");  
 } 
+
+$.fn.extend({          
+    alertWhileClick:function() {            
+          alert('extend');         
+     }       
+});  
